fccaedda66ebee09ff5187f7acd1642ac01c48dd
aklump <sourcecode@intheloftstudios.com>
2016-12-09 15:58:09 -0800

    bugfixes. Added docs to dist.

---
+++ .web_package/hooks/build/50_dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# @file
+# Copy distribution files to /dist
+#
+
+# Allow time for all CodeKit to compile.
+sleep 3
+
+# First, wipe out the dist folder for a clean slate.
+(cd "$7" && (test -d dist && rm -r dist) && mkdir dist)
+
+# Now copy of the necessary folders; don't check first because we want a loud failure.
+rsync -a "$7/core/" "$7/dist/core/"
+rsync -a "$7/public_html/" "$7/dist/docs/"
+
+# ... and files.
+cp "$7/core-version.info" "$7/dist/"
