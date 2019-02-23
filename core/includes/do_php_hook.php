0f9c4d6fd79651aff5186e6a2e3c646075c32f88
Aaron Klump <sourcecode@intheloftstudios.com>
2019-02-22 19:07:31 -0800

    Update getInclude to look in static source files if not found in dynamic.

--- core/includes/do_php_hook.php
+++ core/includes/do_php_hook.php
@@ -15,7 +15,7 @@ require $argv[2] . '/vendor/autoload.php';
 
 // This should be used by the hook files.
 $outline_file = FilePath::create($argv[4], ['install' => FALSE]);
-$compiler = new Compiler(FilePath::create($argv[9]), $outline_file);
+$compiler = new Compiler(FilePath::create($argv[1]), FilePath::create($argv[9]), $outline_file);
 
 // Load the hook file.
 require_once $argv[0];
