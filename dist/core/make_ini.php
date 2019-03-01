ffd97c3ece347d0b1bad3cb4a148b43a4c795666
Aaron Klump <sourcecode@intheloftstudios.com>
2019-03-01 13:42:54 -0800

    Version bumped from 0.10.10 to 0.10.11

--- dist/core/make_ini.php
+++ dist/core/make_ini.php
@@ -1,10 +1,18 @@
 <?php
+
+/**
+ * @file
+ * Generates the advanced help ini file for Drupal advanced help.
+ */
+
 use AKlump\LoftDocs\OutlineToHelpIni;
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 list(, $help_dir, $module_name, $outline_file) = $argv;
 
-$obj = new OutlineToHelpIni($help_dir, $module_name);
-$outline = load_outline($outline_file);
-$obj->setOutline($outline)->generateFile();
+if (file_exists($help_dir)) {
+  $obj = new OutlineToHelpIni($help_dir, $module_name);
+  $outline = load_outline($outline_file);
+  $obj->setOutline($outline)->generateFile();
+}
