41e21e3f095ad17d9540a296fae019f98ca0cbb4
Aaron Klump <sourcecode@intheloftstudios.com>
2020-10-27 15:29:14 -0700

    Fix build step.

--- .web_package/hooks/build/10_scss.sh
+++ .web_package/hooks/build/10_scss.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 bump_sass=$(type sass >/dev/null &2>&1 && which sass)
 
-$bump_sass  --style=compressed --no-cache --update "$7/core/plugins/twig/tpl/sass/style.scss:$7/core/plugins/twig/tpl/style.css"
+$bump_sass  --style=compressed --update "$7/core/plugins/twig/tpl/sass/style.scss:$7/core/plugins/twig/tpl/style.css"
